b397c7322002839bbdd1fe1c6c6f818462fb69b7
AdiAmo95 <adi.amoyal@bellevuecollege.edu>
2024-12-25 20:24:59 -0800

    Initial Commit

---
+++ TodoListApp-SwiftUI/TodoListApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp_SwiftUIApp.swift
+//  TodoListApp-SwiftUI
+//
+//  Created by Adi Amoyal on 25/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodoListApp-SwiftUITests/TodoListApp_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp_SwiftUITests.swift
+//  TodoListApp-SwiftUITests
+//
+//  Created by Adi Amoyal on 25/12/2024.
+//
+
+import Testing
+@testable import TodoListApp_SwiftUI
+
+struct TodoListApp_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
